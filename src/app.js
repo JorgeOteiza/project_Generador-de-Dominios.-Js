7637ef61dd319c553c7af255f10f248ab5e06cc9
Jorge Cancino Oteíza <139603426+JorgeOteiza@users.noreply.github.com>
2024-01-09 02:54:24 +0000

    lógica lista,  generador de nombres de demominios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hola Quilpué");
 };
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+// Array para almacenar las combinaciones de dominios
+let domainNames = [];
+
+// Bucles anidados para combinar pronombres, adjetivos y sustantivos
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      // Concatenar los elementos para formar un nombre de dominio
+      let domainName = pronoun[i] + adj[j] + noun[k] + ".com";
+
+      // Agregar el nombre de dominio al array
+      domainNames.push(domainName);
+    }
+  }
+}
+
+// Imprimir las combinaciones de dominios
+for (let l = 0; l < domainNames.length; l++) {
+  console.log(domainNames[l]);
+}
